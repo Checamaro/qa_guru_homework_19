f13437751f3ba78f0f0e2817f8de42f3f1fd2739
checamar0 <checamar02@gmail.com>
2024-05-25 15:25:31 +0300

    init

--- tests/test_android.py
+++ tests/test_android.py
@@ -12,7 +12,7 @@ def test_search_paris():
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Paris'))
 
-def test_search_python():
+def test_search_london():
     with allure.step('Typing word "London" in search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('London')
