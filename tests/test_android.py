2974f69aa62203350e4543e57dbe282c5b6a074b
checamar0 <checamar02@gmail.com>
2024-05-25 15:25:03 +0300

    Initial commit

---
+++ tests/test_android.py
@@ -0,0 +1,24 @@
+import allure
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+def test_search_paris():
+    with allure.step('Typing word "Paris" in search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Paris')
+
+    with allure.step('Success search'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Paris'))
+
+def test_search_python():
+    with allure.step('Typing word "London" in search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('London')
+
+    with allure.step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('London'))
+        results.first.click()
